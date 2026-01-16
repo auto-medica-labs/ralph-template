7dde1b6d3196c04e06f440f04603dcade34eebbf
Dheerapat Tookkane <dheerapat38@gmail.com>
2026-01-17 02:54:38 +0700

    fix: emphazied on backgorund process kill

--- plan/ralph-once.sh
+++ plan/ralph-once.sh
@@ -18,4 +18,5 @@ If, while implementing the feature, you notice that **ALL** work items \
 is complete, output <promise>COMPLETE</promise>. \
 Let me repeat that again: only output <promise>COMPLETE</promise> when **ALL** work items \
 in @plan/prd.json are completed; otherwise, just exit without outputting anything. \
-Always kill all background processes that you start before exiting the session." --model nvidia/qwen/qwen3-coder-480b-a35b-instruct
+**IMPORTANT** Always kill all background processes that you start before exiting the session. \
+Let me repeat that again: ALWAYS KILL ALL BACKGROUND PROCESSES that you start in each session before exit." --model nvidia/qwen/qwen3-coder-480b-a35b-instruct

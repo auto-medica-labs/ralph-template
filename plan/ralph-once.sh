9ece082c14de1a4a9cc0783965936f5b336ced9a
Dheerapat Tookkane <dheerapat38@gmail.com>
2026-01-16 22:04:32 +0700

    nit: adjust folder format

--- plan/ralph-once.sh
+++ plan/ralph-once.sh
@@ -3,16 +3,16 @@
 set -e
 
 opencode --prompt \
-"Reading the specification from @spec.md and current progress from @progress.txt then \
-1. Decide which task to work on next in @prd.json file. \
+"Reading the specification from @plan/spec.md and current progress from @plan/progress.txt then \
+1. Decide which task to work on next in @plan/prd.json file. \
 This should be the one YOU decide has the highest priority, \
 - not necessarily the first in the list. \
 2. Check any feedback loops, such as types and tests. \
-3. Append your progress to the @progress.txt file. \
-4. Update @prd.json file after each task completed. \
+3. Append your progress to the @plan/progress.txt file. \
+4. Update @plan/prd.json file after each task completed. \
 5. Make a git commit of that feature. \
 ONLY WORK ON A SINGLE FEATURE At A TIME. \
-After you finished each task in @prd.json, exit and let other agent continue. \
+After you finished each task in @plan/prd.json, exit and let other agent continue. \
 If, while implementing the feature, you notice that all work \
 is complete, output <promise>COMPLETE</promise>. \
 " --model openrouter/z-ai/glm-4.7

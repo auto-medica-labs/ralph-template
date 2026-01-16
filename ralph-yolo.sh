950a690871cd67b3cd7a997e0358637ed28b8085
Dheerapat Tookkane <dheerapat.t@tao-bin.com>
2026-01-16 13:15:09 +0700

    fix: adjust ralph prompt

--- ralph-yolo.sh
+++ ralph-yolo.sh
@@ -14,17 +14,17 @@ fi
 # This prompt is basic, we'll expand it later.
 for ((i=1; i<=$1; i++)); do
   result=$(opencode --prompt \
-"@spec.md \
-1. Decide which task to work on next in @prd.json file. \
-This should be the one YOU decide has the highest priority, \
-- not necessarily the first in the list. \
-2. Check any feedback loops, such as types and tests. \
-3. Append your progress to the @progress.txt file. \
-4. Make a git commit of that feature. \
-ONLY WORK ON A SINGLE FEATURE. \
-If, while implementing the feature, you notice that all work \
-is complete, output <promise>COMPLETE</promise>. \
-" --model openrouter/z-ai/glm-4.7)
+  "Reading the specification from @spec.md and current progress from @progress.txt then \
+  1. Decide which task to work on next in @prd.json file. \
+  This should be the one YOU decide has the highest priority, \
+  - not necessarily the first in the list. \
+  2. Check any feedback loops, such as types and tests. \
+  3. Append your progress to the @progress.txt file. \
+  4. Make a git commit of that feature. \
+  ONLY WORK ON A SINGLE FEATURE. \
+  If, while implementing the feature, you notice that all work \
+  is complete, output <promise>COMPLETE</promise>. \
+  " --model openrouter/z-ai/glm-4.7)
 
   echo "$result"
 

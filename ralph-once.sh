1b2188542f3f38d2c34430044b76be175f882386
dheeto <dheerapat38@gmail.com>
2026-01-16 21:17:43 +0700

    fix: update ralph prompt

--- ralph-once.sh
+++ ralph-once.sh
@@ -9,8 +9,10 @@ This should be the one YOU decide has the highest priority, \
 - not necessarily the first in the list. \
 2. Check any feedback loops, such as types and tests. \
 3. Append your progress to the @progress.txt file. \
-4. Make a git commit of that feature. \
-ONLY WORK ON A SINGLE FEATURE. \
+4. Update @prd.json file after each task completed. \
+5. Make a git commit of that feature. \
+ONLY WORK ON A SINGLE FEATURE At A TIME. \
+After you finished each task in @prd.json, exit and let other agent continue. \
 If, while implementing the feature, you notice that all work \
 is complete, output <promise>COMPLETE</promise>. \
 " --model openrouter/z-ai/glm-4.7

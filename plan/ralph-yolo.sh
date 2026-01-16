297123b116bc9433229c522337b1be032c9ac3d0
Dheerapat Tookkane <dheerapat38@gmail.com>
2026-01-17 01:56:39 +0700

    fix: adjust ralph yolo for the same prompt as ralpg once

--- plan/ralph-yolo.sh
+++ plan/ralph-yolo.sh
@@ -14,10 +14,11 @@ git clean -fd
 git restore .
 
 for ((i=1; i<=$1; i++)); do
-  result=$(opencode run --model openrouter/z-ai/glm-4.7 \
-  "Reading the specification from @plan/spec.md and current progress from @plan/progress.txt then \
+  result=$(opencode run --model nvidia/qwen/qwen3-coder-480b-a35b-instruct \
+  "You are typical software engineer, you only work for a narrow scoped that you been told to do, nothing more, nothing less. \
+  Reading the specification from @plan/spec.md and current progress from @plan/progress.txt then \
   1. Decide which task to work on next in @plan/prd.json file. \
-  This should be the one YOU decide has the highest priority, \
+  This should be the one YOU decide has the highest priority \
   - not necessarily the first in the list. \
   2. Check any feedback loops, such as types and tests. \
   3. Append your progress to the @plan/progress.txt file. \
@@ -25,8 +26,11 @@ for ((i=1; i<=$1; i++)); do
   5. Make a git commit of that feature. \
   ONLY WORK ON A SINGLE FEATURE At A TIME. \
   After you finished each task in @plan/prd.json, exit and let other agent continue. \
-  If, while implementing the feature, you notice that all work \
-  is complete, output <promise>COMPLETE</promise>.")
+  If, while implementing the feature, you notice that **ALL** work items \
+  is complete, output <promise>COMPLETE</promise>. \
+  Let me repeat that again: only output <promise>COMPLETE</promise> when **ALL** work items \
+  in @plan/prd.json are completed; otherwise, just exit without outputting anything. \
+  Always kill all background processes that you start before exiting the session.")
 
   echo "$result"
 

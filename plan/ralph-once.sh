63126a8a686fde510b8bf858f18de1e725dda666
dheeto <dheerapat38@gmail.com>
2026-01-17 22:53:19 +0700

    nit: refine prompt

--- plan/ralph-once.sh
+++ plan/ralph-once.sh
@@ -14,11 +14,13 @@ This should be the one YOU decide has the highest priority \
 5. Make a git commit of that feature. \
 ONLY WORK ON A SINGLE FEATURE At A TIME. \
 After you finished each task in PWD/plan/prd.json, exit and let other agent continue. \
-If, while implementing the feature, you notice that **ALL** work items \
+**IMPORTANT** \
+1. If, while implementing the feature, you notice that **ALL** work items \
 is complete, output <promise>COMPLETE</promise>. \
-Let me repeat that again: only output <promise>COMPLETE</promise> when **ALL** work items \
+2. When testing the implementation, If you wish to run a long running background process, always add timeout. \
+3. Always kill all background processes that you start before exiting the session. \
+Let me repeat that again. \
+1. Only output <promise>COMPLETE</promise> when **ALL** work items \
 in PWD/plan/prd.json are completed; otherwise, just exit without outputting anything. \
-**IMPORTANT** When testing the implementation, If you wish to run a long running background process, always add timeout. \
-And always kill all background processes that you start before exiting the session. \
-Let me repeat that again: **EVERY LONG-RUNNING BACKGROUND PROCESS MUST HAVE TIMEOUT** and \
-**ALWAYS KILL ALL BACKGROUND PROCESSES** that you start in each session before exit." --model nvidia/qwen/qwen3-coder-480b-a35b-instruct
+2. **EVERY LONG-RUNNING BACKGROUND PROCESS MUST HAVE TIMEOUT** and \
+3. **ALWAYS KILL ALL BACKGROUND PROCESSES** that you start in each session before exit." --model nvidia/qwen/qwen3-coder-480b-a35b-instruct
--- plan/ralph-yolo.sh
+++ plan/ralph-yolo.sh
@@ -32,14 +32,16 @@ for ((i=1; i<=$1; i++)); do
   5. Make a git commit of that feature. \
   ONLY WORK ON A SINGLE FEATURE At A TIME. \
   After you finished each task in PWD/plan/prd.json, exit and let other agent continue. \
-  If, while implementing the feature, you notice that **ALL** work items \
+  **IMPORTANT** \
+  1. If, while implementing the feature, you notice that **ALL** work items \
   is complete, output <promise>COMPLETE</promise>. \
-  Let me repeat that again: only output <promise>COMPLETE</promise> when **ALL** work items \
+  2. When testing the implementation, If you wish to run a long running background process, always add timeout. \
+  3. Always kill all background processes that you start before exiting the session. \
+  Let me repeat that again. \
+  1. Only output <promise>COMPLETE</promise> when **ALL** work items \
   in PWD/plan/prd.json are completed; otherwise, just exit without outputting anything. \
-  **IMPORTANT** When testing the implementation, If you wish to run a long running background process, always add timeout. \
-  And always kill all background processes that you start before exiting the session. \
-  Let me repeat that again: **EVERY LONG-RUNNING BACKGROUND PROCESS MUST HAVE TIMEOUT** and \
-  **ALWAYS KILL ALL BACKGROUND PROCESSES** that you start in each session before exit.")
+  2. **EVERY LONG-RUNNING BACKGROUND PROCESS MUST HAVE TIMEOUT** and \
+  3. **ALWAYS KILL ALL BACKGROUND PROCESSES** that you start in each session before exit.")
 
   echo "$result"
 
